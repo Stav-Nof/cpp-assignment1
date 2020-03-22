29a6eae187e1a640d185e0175c671f3e68b328bc
yarin shapira <yariniii19@gmail.com>
2020-03-22 19:42:05 +0200

    adding Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,89 @@
+#include "doctest.h"
+#include "PhoneticFinder.hpp"
+using namespace phonetic;
+
+#include <string>
+using namespace std;
+
+
+
+TEST_CASE("Test replacement of v and w") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+    /* Add more checks here */
+}
+
+TEST_CASE("Test replacement of b and f and p") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+    /* Add more checks here */
+}
+
+TEST_CASE("Test replacement of g and j") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+    /* Add more checks here */
+}
+
+TEST_CASE("Test replacement of c and k and q") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+    /* Add more checks here */
+}
+
+TEST_CASE("Test replacement of s and z") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+    /* Add more checks here */
+}
+
+TEST_CASE("Test replacement of d and t") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+    /* Add more checks here */
+}
+
+TEST_CASE("Test replacement of o and u") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+    /* Add more checks here */
+}
+
+TEST_CASE("Test replacement of i and y") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+    /* Add more checks here */
+}
+
+TEST_CASE("Test replacement of lower-case and upper-case") {
+    string text = "Happi xxx yyy";
+    CHECK(find(text, "happi") == string("Happi"));
+    CHECK(find(text, "Happi") == string("Happi"));
+    CHECK(find(text, "HAPPI") == string("Happi"));
+    CHECK(find(text, "HaPpI") == string("Happi"));
+    /* Add more checks here */
+}
